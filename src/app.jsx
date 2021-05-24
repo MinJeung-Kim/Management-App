09f70d0574b874da87c52a9925eeabdcc643fd19
MinJeung-Kim <focso5@knou.ac.kr>
2021-05-24 11:28:59 +0900

    add login and logout functionality

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import styles from './app.module.css';
 import Login from './components/login/login';
+import Maker from './components/maker/maker';
 
 function App({ authService }) {
   return (
     <div className={styles.app}>
-      <Login authService={authService} />
+      <BrowserRouter>
+        <Switch>
+          {/* 경로가 정확히 '/'일때는 로그인 화면으로 이동 */}
+          <Route exact path="/">
+            <Login authService={authService} />
+          </Route>
+          <Route exact path="/maker">
+            <Maker authService={authService} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -9,6 +9,18 @@ class AuthService {
     //팝업으로 로그인창 활성화
     return firebaseApp.auth().signInWithPopup(authProvider);
   }
+
+  logout() {
+    firebase.auth().signOut();
+  }
+
+  //사용자가 바꼈을때 원하는 기능을 수행하는 콜백 함수.
+  onAuthChange(onUserChanged) {
+    // 사용자 정보 전달
+    firebase.auth().onAuthStateChanged((user) => {
+      onUserChanged(user);
+    });
+  }
 }
 
 export default AuthService;
