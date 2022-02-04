b15c980e9de53c2b893ce558db73f1de6fe0039f
김민정 <kimminjeung@wizzpay.co.kr>
2022-02-04 12:44:13 +0900

    Add function Pagination

--- src/components/user_list_table/user_list_table.jsx
+++ src/components/user_list_table/user_list_table.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import Pagination from "../pagination/Pagination";
 import styles from "./user_list_table.module.css";
 
 const DEFAULT_IMAGE = "/images/default_logo.png";
+
 const UserListTable = ({ cards, openEditor, openDetail }) => {
+  const [limit, setLimit] = useState(3);
+  const [page, setPage] = useState(1);
+  const offset = (page - 1) * limit;
+
   const fnOpenEditor = () => {
     openEditor(true);
   };
@@ -37,28 +43,54 @@ const UserListTable = ({ cards, openEditor, openDetail }) => {
           </tr>
         </thead>
         <tbody className={styles.tbody}>
-          {Object.keys(cards).map((key) => (
-            <tr key={key}>
-              <td className={styles.profile}>
-                <img src={cards[key].fileURL || DEFAULT_IMAGE} alt="profile" />
-                {cards[key].name}
-              </td>
-              <td> {cards[key].age}</td>
-              <td>
-                {cards[key].phone
-                  .replace(/[^0-9]/, "")
-                  .replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`)}
-              </td>
-              <td>{cards[key].job}</td>
-              <td>일반</td>
-              <td>{cards[key].registration}</td>
-              <td> {cards[key].period}</td>
-              <td>
-                <i className="fas fa-pencil-alt" onClick={fnOpenDetail}></i>
-              </td>
-            </tr>
-          ))}
+          {Object.keys(cards)
+            .slice(offset, offset + limit)
+            .map((key) => (
+              <tr key={key}>
+                <td className={styles.profile}>
+                  <img
+                    src={cards[key].fileURL || DEFAULT_IMAGE}
+                    alt="profile"
+                  />
+                  {cards[key].name}
+                </td>
+                <td> {cards[key].age}</td>
+                <td>
+                  {cards[key].phone
+                    .replace(/[^0-9]/, "")
+                    .replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`)}
+                </td>
+                <td>{cards[key].job}</td>
+                <td>일반</td>
+                <td>{cards[key].registration}</td>
+                <td> {cards[key].period}</td>
+                <td>
+                  <i className="fas fa-pencil-alt" onClick={fnOpenDetail}></i>
+                </td>
+              </tr>
+            ))}
         </tbody>
+        <label>
+          페이지 당 표시할 게시물 수:&nbsp;
+          <select
+            type="number"
+            value={limit}
+            onChange={({ target: { value } }) => setLimit(Number(value))}
+          >
+            <option value="3">3</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+            <option value="20">20</option>
+          </select>
+        </label>
+        <footer>
+          <Pagination
+            total={Object.keys(cards).length}
+            limit={limit}
+            page={page}
+            setPage={setPage}
+          />
+        </footer>
       </table>
       <i className="fas fa-edit" onClick={fnOpenEditor}></i>
     </div>
