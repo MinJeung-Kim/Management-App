197f62d0571587752bb9cd415bd8ba5f8ffd0688
김민정 <kimminjeung@wizzpay.co.kr>
2022-02-07 13:22:25 +0900

    D-Day 기능 추가

--- src/components/dday_count_down/dday_count_down.jsx
+++ src/components/dday_count_down/dday_count_down.jsx
@@ -1,14 +1,40 @@
+import { useEffect, useState } from "react";
+
 const DdayCountDown = ({ registration, period }) => {
-  const month = period.charAt(0);
-  const date = new Date(registration);
-  const dday = date.setMonth(date.getMonth() + month);
+  const dday = new Date(registration);
+  const yyyy = dday.getFullYear();
+  const mm = dday.getMonth() + 1 + parseInt(period);
+  const dd = dday.getDate();
 
-  const today = new Date(registration).getTime();
-  const gap = dday - today;
-  const day = Math.ceil(gap / (1000 * 60 * 60 * 24));
+  let deadline = `${yyyy}-${mm}-${dd}`;
 
-  console.log("D-DAY까지 " + day + "일 ");
-  return <p>D-DAY {day}</p>;
+  const [days, setDays] = useState(0);
+
+  const leading0 = (num) => {
+    return num < 10 ? "0" + num : num;
+  };
+
+  const getTimeUntil = (deadline) => {
+    const time = Date.parse(deadline) - Date.parse(new Date());
+    if (time < 0) {
+      setDays(0);
+    } else {
+      setDays(Math.floor(time / (1000 * 60 * 60 * 24)));
+    }
+  };
+
+  useEffect(() => {
+    setInterval(() => getTimeUntil(deadline), 10000);
+
+    return () => getTimeUntil(deadline);
+  }, [deadline]);
+
+  console.log(leading0(days));
+  return (
+    <div>
+      <div className="Clock-days">D-{leading0(days)} </div>
+    </div>
+  );
 };
 
 export default DdayCountDown;
